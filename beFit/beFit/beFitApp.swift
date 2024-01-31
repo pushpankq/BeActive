f038f0054f7c8b40d09c291eab968090815c87cf
pushpank <pushpankq@gmail.com>
2024-01-31 22:58:23 +0530

    second commit

---
+++ beFit/beFit/beFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  beFitApp.swift
+//  beFit
+//
+//  Created by Pushpank Kumar on 31/01/24.
+//
+
+import SwiftUI
+
+@main
+struct beFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
